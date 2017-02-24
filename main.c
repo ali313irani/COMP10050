d167e7270601ae5ba00a63ae566b086ce7e29be6
Nebojsa Cvetkovic <nebkat@gmail.com>
2017-02-24 11:42:05 +0000

    Added structs

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+typedef enum Slot {
+	Ground, City, Hill
+} Slot;
+
+typedef enum PlayerType {
+	Elf, Human, Ogre, Wizard
+} PlayerType;
+
+typedef struct Player {
+	char *name;
+	enum PlayerType type;
+	int life;
+	int smartness;
+	int strength;
+	int magic;
+	int luck;
+	int dexterity;
+} Player;
+
 int main(void) {
 	printf("Hello world!");
 }
