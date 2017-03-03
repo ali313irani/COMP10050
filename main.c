845c3201bd8eab91fd3c0d5a5364d9388f7d0630
Nebojsa Cvetkovic <nebkat@gmail.com>
2017-03-03 11:34:24 +0000

    Shuffle player positions

--- main.c
+++ main.c
@@ -21,7 +21,9 @@ typedef struct Player {
 	int dexterity;
 } Player;
 
+void swap(void *a, void *b, size_t size);
 unsigned int rand_range(int min, int max);
+void shuffle(void *array, size_t num, size_t size);
 
 char * slotName(Slot s) {
     switch (s) {
@@ -92,7 +94,14 @@ int main(void) {
 	
 	printf("Enter number of slots(max = 20): \n");
 	scanf("%d", &n);
-	
+
+    int player_positions[20] = {-1};
+    for (i = 0; i < players_count; i++) {
+        player_positions[i] = i;
+    }
+    shuffle(player_positions, (size_t) n, sizeof(int));
+
+    
 	for(i = 0; i > n; i++)
 	{
 		int r = rand_range(0, 2);
@@ -107,9 +116,22 @@ int main(void) {
 			slots[i] = Hill;
 		}
 	}
-	
-	
 
+}
+
+/**
+ * Swaps the values at two pointers
+ *
+ * @param a Pointer a.
+ * @param b Pointer b.
+ * @param size Size of value pointed at by a and b.
+ */
+void swap(void *a, void *b, size_t size) {
+    void* temp = malloc(size);
+    memcpy(temp, a, size);
+    memcpy(a, b, size);
+    memcpy(b, temp, size);
+    free(temp);
 }
 
 /**
@@ -129,4 +151,20 @@ unsigned int rand_range(int min, int max) {
 	} while (r >= limit);
 
 	return min + (unsigned int) (r / buckets);
+}
+
+/**
+ * Shuffles an array using the Fisher–Yates algorithm
+ * @param array Pointer to the first element of the array to be shuffled.
+ * @param num Number of elements in the array.
+ * @param size Size in bytes of each element in the array.
+ */
+void shuffle(void *array, size_t num, size_t size) {
+    for (size_t i = 0; i < num; i++) {
+        size_t j = i + rand_range(num - i - 1);
+
+        if (i != j) {
+            swap(array + (i * size), array + (j * size), size);
+        }
+    }
 }
